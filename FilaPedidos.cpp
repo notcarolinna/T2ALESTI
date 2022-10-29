9d77551cf781c680e34a1abc885ac3a27ad16597
Carolina Nóbrega <notcarolinna@Gmail.com>
2022-10-28 21:22:56 -0300

    Update FilaPedidos.cpp

--- FilaPedidos.cpp
+++ FilaPedidos.cpp
@@ -6,24 +6,35 @@ int TAM_MAX = 10;
 
 FilaPedidos::FilaPedidos()
 {
-    this->pedidos = (Pedido **)malloc(TAM_MAX * sizeof(int)); // alocação de um bloco de memória de tamanho 5 de pedidos, caso atingir 5, duplica o tamanho do bloco
+    // this->pedidos = (Pedido**)malloc(TAM_MAX * sizeof(int)); // aloca��o de um bloco de mem�ria de tamanho 5 de pedidos, caso atingir 5, duplica o tamanho do bloco
+
+    this->pedidos = new Pedido *[TAM_MAX];
     this->tamanho = 0;
 }
 
-void FilaPedidos::enqueue(Pedido* elemento)
+void FilaPedidos::enqueue(Pedido *elemento)
 {
+    
+    //Criação de uma nova lista com tam max e o for copia e passa a lista como
+    
     if (tamanho >= TAM_MAX)
     {
         TAM_MAX += 10;
-        pedidos = (Pedido **)realloc(pedidos, TAM_MAX * sizeof(int));
+        Pedido **newPedidos = new Pedido *[TAM_MAX];
+        for (int i = 0; i < sizeof(pedidos); i++)
+        {
+            newPedidos[i] = pedidos[i];
+        }
+        delete pedidos; // o delete tá aqui pra deletar o bloco de memória alocada
+        pedidos = newPedidos;
     }
     pedidos[tamanho] = elemento;
     tamanho++;
 }
 
-Pedido* FilaPedidos::dequeue()
+Pedido *FilaPedidos::dequeue()
 {
-    Pedido* pedido = pedidos[0];
+    Pedido *pedido = pedidos[0];
     for (int i = 1; i <= tamanho; i++)
     {
         pedidos[i - 1] = pedidos[i];
