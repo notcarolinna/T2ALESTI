59cd577a9216a1f287432888db86af745052e210
Carolina Nóbrega <notcarolinna@Gmail.com>
2022-10-30 17:18:15 -0300

    Update Separadores.cpp

--- Separadores.cpp
+++ Separadores.cpp
@@ -9,3 +9,63 @@ Separadores::Separadores(int numSeparadores)
     this->numSeparadores = numSeparadores;            // definindo o número de separadores
     this->separadoresOcupados = 0;
 }
+
+void Separadores::adicionaPedido(Pedido *pedido, int tempo)
+{
+    if (separadoresOcupados < numSeparadores)
+    {
+        for (int i = 0; i < numSeparadores; i++)
+        {
+            if (separadores[i] == NULL)
+            {
+                separadores[separadoresOcupados] = pedido;
+                separadoresOcupados++;
+                pedido->addTempoGastoFila(pedido->getTempoPronto() - tempo);
+                pedido->setTempoPronto(tempo + pedido->getProdutos());
+
+                return;
+            }
+        }
+    }
+}
+
+Pedido *Separadores::removePedidoPronto(int tempo)
+{
+    for (int i = 0; i < numSeparadores; i++)
+    {
+        if (separadores[i] != NULL)
+        {
+
+            if (separadores[i]->getTempoPronto() == tempo)
+            {
+                Pedido *pedido = separadores[i];
+                separadores[i] = NULL;
+                separadoresOcupados--;
+
+                return pedido;
+            }
+        }
+    }
+
+    return NULL;
+}
+
+bool Separadores::existePedidoPronto(int tempo)
+{
+    for (int i = 0; i < numSeparadores; i++)
+    {
+        if (separadores[i] != NULL)
+        {
+
+            if (separadores[i]->getTempoPronto() == tempo)
+            {
+                return true;
+            }
+        }
+    }
+}
+
+bool Separadores::existeSeparadorLivre()
+{
+    return numSeparadores != separadoresOcupados;
+}
