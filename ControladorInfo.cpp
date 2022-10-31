e2c3c45e0bcfde043ea52bc7df22a84ec8544765
Carolina Nóbrega <notcarolinna@Gmail.com>
2022-10-31 00:54:08 -0300

    Create ControladorInfo.cpp

---
+++ ControladorInfo.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "ControladorInfo.hpp"
+using namespace std;
+
+ControladorInfo::ControladorInfo()
+{
+    this->numPedidosCriados = 0;
+    this->numPedidosCancelados = 0;
+    this->numPedidosEnviados = 0;
+}
+
+void ControladorInfo::addPedidoCriado()
+{
+    numPedidosCriados++;
+}
+
+void ControladorInfo::addPedidoCancelado()
+{
+    numPedidosCancelados++;
+}
+
+void ControladorInfo::addPedidoEnviado()
+{
+    numPedidosEnviados++;
+}
+
+int ControladorInfo::getPedidosCriados()
+{
+    return numPedidosCriados;
+}
+
+int ControladorInfo::getPedidosCancelados()
+{
+    return numPedidosCancelados;
+}
+
+int ControladorInfo::getPedidosEnviados()
+{
+    return numPedidosEnviados;
+}
