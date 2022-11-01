e51184f75a1acc212b941f95c0271d2fbe1ca68c
Carolina Nóbrega <notcarolinna@Gmail.com>
2022-11-01 02:06:25 -0300

    Update FilaPedidos.cpp

--- FilaPedidos.cpp
+++ FilaPedidos.cpp
@@ -6,7 +6,6 @@ int TAM_MAX = 10;
 
 FilaPedidos::FilaPedidos()
 {
-    // this->pedidos = (Pedido**)malloc(TAM_MAX * sizeof(int)); // aloca��o de um bloco de mem�ria de tamanho 5 de pedidos, caso atingir 5, duplica o tamanho do bloco
 
     this->pedidos = new Pedido *[TAM_MAX];
     this->tamanho = 0;
@@ -14,9 +13,7 @@ FilaPedidos::FilaPedidos()
 
 void FilaPedidos::enqueue(Pedido *elemento)
 {
-    
-    //Criação de uma nova lista com tam max e o for copia e passa a lista como
-    
+
     if (tamanho >= TAM_MAX)
     {
         TAM_MAX += 10;
@@ -47,3 +44,10 @@ int FilaPedidos::size()
 {
     return tamanho;
 }
+
+void FilaPedidos::probCancelamento(int probCancelamento)
+{
+    for (int i = 0; i < tamanho; i++)
+    {
+        pedidos[i]->probCancelamento(probCancelamento);
+    }
