2d5e5756a518fa11cf503fc09a517f56b1d63636
Carolina Nóbrega <notcarolinna@Gmail.com>
2022-11-01 02:04:56 -0300

    Update pedido.hpp

--- pedido.hpp
+++ pedido.hpp
@@ -6,13 +6,16 @@ private:
     int produtos;
     int tempoPronto;    // quando está pronto
     int tempoGastoFila; // diferença entre entradar e sair da fila
+    bool pedidoCancelado;
 
 public:
     Pedido(int produtos);
 
-    int getProdutos();                 // retorna a quantidade de produto
-    int getTempoPronto();              // retorna quando estiver pronto
-    void setTempoPronto(int tempo);    // modifica o tempo pronto
-    int getTempoGastoFila();           // retorna o tempo gasto em fila
-    void addTempoGastoFila(int tempo); // adicionando tempo
+    int getProdutos();                           // retorna a quantidade de produto
+    int getTempoPronto();                        // retorna quando estiver pronto
+    void setTempoPronto(int tempo);              // modifica o tempo pronto
+    int getTempoGastoFila();                     // retorna o tempo gasto em fila
+    void addTempoGastoFila(int tempo);           // adicionando tempo
+    void probCancelamento(int probCancelamento); // probabilidade de um pedido ser cancelado
+    bool isPedidoCancelado();                    // verifica se o pedido conseguiu ser cancelado
 };
