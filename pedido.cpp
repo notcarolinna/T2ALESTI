6af0aca39f59ee9a0dea6ce0cc0d3e4a4eb69d6c
Carolina Nóbrega <notcarolinna@Gmail.com>
2022-11-02 05:05:04 -0300

    Update pedido.cpp

--- pedido.cpp
+++ pedido.cpp
@@ -1,51 +1,46 @@
 #include <iostream>
-#include "pedido.hpp"
+#include "Pedido.h"
 
 using namespace std;
 
-Pedido::Pedido(int produtos)
-{
-
-    this->produtos = produtos;
-    this->tempoGastoFila = 0; // inicializa em 0
-    this->tempoPronto = 0;
-    this->pedidoCancelado = false;
+Pedido::Pedido(int produtos) {
+	this->produtos = produtos;
+	this->tempoGastoFila = 0;
+	this->tempoPronto = 0;
+	this->pedidoCancelado = false;
 }
 
-int Pedido::getProdutos()
-{
-    return produtos;
+int Pedido::getProdutos() {
+	return produtos;
 }
 
-void Pedido::setTempoPronto(int tempo)
-{
-    tempoPronto = tempo; // definindo que tempo pronto � tempo ;)
+void Pedido::setTempoPronto(int tempo) {
+	tempoPronto = tempo;
 }
 
-int Pedido::getTempoPronto()
-{
-    return tempoPronto;
+int Pedido::getTempoPronto() {
+	return tempoPronto;
 }
 
-void Pedido::addTempoGastoFila(int tempo)
-{
-    tempoGastoFila += tempo;
+void Pedido::addTempoGastoFila(int tempo) {
+	tempoGastoFila += tempo;
 }
 
-int Pedido::getTempoGastoFila()
-{
-    return tempoGastoFila;
+int Pedido::getTempoGastoFila() {
+	return tempoGastoFila;
 }
 
 void Pedido::probCancelamento(int probCancelamento)
 {
-    if (rand() % (probCancelamento + 1))
-    {
-        probCancelamento = true;
-    }
+	if (rand() % (probCancelamento + 1))
+	{
+		pedidoCancelado = true;
+	}
 }
 
 bool Pedido::isPedidoCancelado()
 {
-    return pedidoCancelado;
+	return pedidoCancelado;
 }
+
+
