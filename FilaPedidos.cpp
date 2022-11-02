32588bebeb0ad7f1a5adac7a3fd98e3cbad1d6cd
Carolina Nóbrega <notcarolinna@Gmail.com>
2022-11-02 05:04:47 -0300

    Update FilaPedidos.cpp

--- FilaPedidos.cpp
+++ FilaPedidos.cpp
@@ -1,53 +1,64 @@
 #include <iostream>
-#include "FilaPedidos.hpp";
+
+#include "FilaPedidos.h"
+
 using namespace std;
 
 int TAM_MAX = 10;
 
-FilaPedidos::FilaPedidos()
-{
-
-    this->pedidos = new Pedido *[TAM_MAX];
-    this->tamanho = 0;
+FilaPedidos::FilaPedidos() {
+	this->pedidos = new Pedido * [TAM_MAX];
+	this->tamanho = 0;
 }
 
-void FilaPedidos::enqueue(Pedido *elemento)
+void FilaPedidos::enqueue(Pedido* elemento)
 {
+	if (tamanho >= TAM_MAX) {
+		TAM_MAX += 10;
 
-    if (tamanho >= TAM_MAX)
-    {
-        TAM_MAX += 10;
-        Pedido **newPedidos = new Pedido *[TAM_MAX];
-        for (int i = 0; i < sizeof(pedidos); i++)
-        {
-            newPedidos[i] = pedidos[i];
-        }
-        delete pedidos; // o delete tá aqui pra deletar o bloco de memória alocada
-        pedidos = newPedidos;
-    }
-    pedidos[tamanho] = elemento;
-    tamanho++;
+		Pedido** newPedidos = new Pedido * [TAM_MAX];
+
+		for (int i = 0; i < tamanho; i++)
+		{
+			newPedidos[i] = pedidos[i];
+		}
+
+		// delete pedidos;
+		pedidos = newPedidos;
+	}
+
+	pedidos[tamanho] = elemento;
+	tamanho++;
 }
 
-Pedido *FilaPedidos::dequeue()
+Pedido* FilaPedidos::dequeue()
 {
-    Pedido *pedido = pedidos[0];
-    for (int i = 1; i <= tamanho; i++)
-    {
-        pedidos[i - 1] = pedidos[i];
-    }
-    tamanho--;
-    return pedido;
+	Pedido* pedido = pedidos[0];
+	for (int i = 1; i <= tamanho; i++) {
+		pedidos[i - 1] = pedidos[i];
+	}
+	tamanho--;
+	return pedido;
 }
 
 int FilaPedidos::size()
 {
-    return tamanho;
+	return tamanho;
+}
+
+bool FilaPedidos::isEmpty()
+{
+	return tamanho == 0;
 }
 
 void FilaPedidos::probCancelamento(int probCancelamento)
 {
-    for (int i = 0; i < tamanho; i++)
-    {
-        pedidos[i]->probCancelamento(probCancelamento);
-    }
+	for (int i = 0; i < tamanho; i++)
+	{
+		pedidos[i]->probCancelamento(probCancelamento);
+	}
+}
+
+
+
+
