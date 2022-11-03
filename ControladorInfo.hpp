168b975278fd4cbc18ee9d85749a1a608880ea7e
Carolina Nóbrega <notcarolinna@Gmail.com>
2022-11-03 01:21:21 -0300

    Update ControladorInfo.hpp

--- ControladorInfo.hpp
+++ ControladorInfo.hpp
@@ -6,14 +6,24 @@ private:
 	int numPedidosCriados;
 	int numPedidosCancelados;
 	int numPedidosEnviados;
+	int numPedidosQuaseCancelados;
+	int tempoFilaPedidos;
+	int pedidoMaisDemorado;
+
 public:
 	ControladorInfo();
 
 	void addPedidoCriado();
 	void addPedidoCancelado();
 	void addPedidoEnviado();
+	void addPedidoQuaseCancelado();
+	void addTempoFilaPedidos(int tempo);
+	void setPedidoMaisDemorado(int tempo);
 
 	int getPedidosCriados();
 	int getPedidosCancelados();
 	int getPedidosEnviados();
+	int getPedidoQuaseCancelados();
+	float getMediaTempoFilaPedidos();
+	int getPedidoMaisDemorado();
 };
