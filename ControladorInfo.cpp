e7e69aa9628bf068d5ec80a914aa67b43a38535d
Carolina Nóbrega <notcarolinna@Gmail.com>
2022-11-03 01:20:57 -0300

    Update ControladorInfo.cpp

--- ControladorInfo.cpp
+++ ControladorInfo.cpp
@@ -8,6 +8,9 @@ ControladorInfo::ControladorInfo()
 	this->numPedidosCriados = 0;
 	this->numPedidosCancelados = 0;
 	this->numPedidosEnviados = 0;
+	this->numPedidosQuaseCancelados = 0;
+	this->tempoFilaPedidos = 0;
+	this->pedidoMaisDemorado = 0;
 }
 
 void ControladorInfo::addPedidoCriado()
@@ -25,6 +28,25 @@ void ControladorInfo::addPedidoEnviado()
 	numPedidosEnviados++;
 }
 
+void ControladorInfo::addTempoFilaPedidos(int tempo)
+{
+	tempoFilaPedidos += tempo;
+}
+
+
+void ControladorInfo::addPedidoQuaseCancelado() {
+	numPedidosQuaseCancelados++;
+}
+
+void ControladorInfo::setPedidoMaisDemorado(int tempo)
+{
+	if (tempo > pedidoMaisDemorado)
+	{
+		pedidoMaisDemorado = tempo;
+	}
+}
+
+
 int ControladorInfo::getPedidosCriados()
 {
 	return numPedidosCriados;
@@ -39,3 +61,21 @@ int ControladorInfo::getPedidosEnviados()
 {
 	return numPedidosEnviados;
 }
+
+int ControladorInfo::getPedidoQuaseCancelados()
+{
+	return numPedidosQuaseCancelados;
+}
+
+float ControladorInfo::getMediaTempoFilaPedidos()
+{
+	return (float) tempoFilaPedidos / (float) numPedidosEnviados;
+}
+
+int ControladorInfo::getPedidoMaisDemorado()
+{
+	return pedidoMaisDemorado;
+}
+
+
+
